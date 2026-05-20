7a457c1c5131b9194861c8c138ff1874597c1f97
Agustin <albertofernandez11905@gmail.com>
2026-05-20 16:09:32 -0300

    Ejercicio 2

--- ejercicios/ejercicios_nodejs/index.js
+++ ejercicios/ejercicios_nodejs/index.js
@@ -7,3 +7,15 @@ import chalk from 'chalk';
 console.log(chalk.green('✔ Éxito: El proceso se completó correctamente.'));
 console.log(chalk.red('✖ Error: Algo salió mal en el sistema.'));
 console.log(chalk.yellow('⚠ Advertencia: Revisa la configuración antes de seguir.'));
+
+
+// Ejercicio 2.
+
+import cowsay from 'cowsay';
+
+console.log(cowsay.say({
+    text: "¡Hola, soy Agustín programando en Node.js!",
+    e: "oO", 
+    T: "U "  
+}));
+
