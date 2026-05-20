4f19a9d62def0c829a0c139a7c46c27072a18bc7
Agustin <albertofernandez11905@gmail.com>
2026-05-20 16:19:15 -0300

    Ejercicio 3

--- ejercicios/ejercicios_nodejs/index.js
+++ ejercicios/ejercicios_nodejs/index.js
@@ -2,6 +2,7 @@ console.log("¡Todo pronto! Proyecto inicializado con pnpm v11.1.3");
 
 
 // Ejercicio 1.
+
 import chalk from 'chalk';
 
 console.log(chalk.green('✔ Éxito: El proceso se completó correctamente.'));
@@ -19,3 +20,30 @@ console.log(cowsay.say({
     T: "U "  
 }));
 
+
+// Ejercicio 3.
+
+import axios from 'axios';
+
+const pokemonName = 'pikachu';
+
+console.log(chalk.cyan(`\nBuscando datos de ${pokemonName} en la nube... 🔍`));
+
+axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(response => {
+        const pokemonData = response.data;
+
+        console.log(chalk.yellow.bold('\n==============================='));
+        console.log(chalk.green.bold(`   📊 DATOS DE: ${pokemonData.name.toUpperCase()} `));
+        console.log(chalk.yellow.bold('==============================='));
+        
+        // Lo que pide el profesor: nombre, altura y peso
+        console.log(`${chalk.blue.bold('Nombre:')} ${pokemonData.name}`);
+        console.log(`${chalk.blue.bold('Altura:')} ${pokemonData.height} decímetros`);
+        console.log(`${chalk.blue.bold('Peso:')} ${pokemonData.weight} hectogramos`);
+        
+        console.log(chalk.yellow.bold('===============================\n'));
+    })
+    .catch(error => {
+        console.error(chalk.red('✖ Error al conectar con la PokeAPI:'), error.message);
+    });
