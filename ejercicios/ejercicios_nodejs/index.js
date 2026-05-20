7619e62927710365006cfc73ec8408ade220e691
Agustin <albertofernandez11905@gmail.com>
2026-05-20 15:43:39 -0300

    Ejercicio 1

--- ejercicios/ejercicios_nodejs/index.js
+++ ejercicios/ejercicios_nodejs/index.js
@@ -1,1 +1,9 @@
 console.log("¡Todo pronto! Proyecto inicializado con pnpm v11.1.3");
+
+
+// Ejercicio 1.
+import chalk from 'chalk';
+
+console.log(chalk.green('✔ Éxito: El proceso se completó correctamente.'));
+console.log(chalk.red('✖ Error: Algo salió mal en el sistema.'));
+console.log(chalk.yellow('⚠ Advertencia: Revisa la configuración antes de seguir.'));
